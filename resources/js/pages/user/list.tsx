873064eef327a03ce0828c7e53aaa33034496014
Mina Shoaib Rahman <shoaibsaikat@gmail.com>
2026-04-03 08:42:41 +0600

    removed inappropriate form usage, need to apply form properly

--- resources/js/pages/user/list.tsx
+++ resources/js/pages/user/list.tsx
@@ -18,7 +18,7 @@ export default function UserList({
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="User List" />
             <div className="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4">
-                <table className="table">
+                <table className="table table-primary">
                     <thead>
                         <tr>
                             <th scope="col">#</th>
@@ -26,39 +26,25 @@ export default function UserList({
                             <th scope="col">Designation</th>
                             <th scope="col">Manage User</th>
                             <th scope="col">Manage Leave</th>
+                            <th scope="col">Action</th>
                         </tr>
                     </thead>
                     <tbody>
                         {users.map((user) => (
-                            // <Form action="#" method="post">
-                            //     {({
-                            //         errors,
-                            //         hasErrors,
-                            //         processing,
-                            //         progress,
-                            //         wasSuccessful,
-                            //         recentlySuccessful,
-                            //         setError,
-                            //         clearErrors,
-                            //         resetAndClearErrors,
-                            //         defaults,
-                            //         isDirty,
-                            //         reset,
-                            //         submit,
-                            //     }) => (
-                            <tr>
+                            <tr key={user.id}>
                                 <th scope="row">{user.id}</th>
                                 <td>{user.name}</td>
                                 <td>{user.designation}</td>
                                 <td className="form-check">
-                                    <input className="form-check-input" id="can_manage_user" type="checkbox" name="can_manage_user_checkbox" defaultChecked={user.can_manage_user === true} />
+                                    <input className="form-check-input" id="can_manage_user" type="checkbox" name="can_manage_user" defaultChecked={user.can_manage_user === true} />
                                 </td>
                                 <td className="form-check">
-                                    <input className="form-check-input" id="can_manage_leave" type="checkbox" name="can_manage_leave_checkbox" defaultChecked={user.can_manage_leave === true} />
+                                    <input className="form-check-input" id="can_manage_leave" type="checkbox" name="can_manage_leave" defaultChecked={user.can_manage_leave === true} />
+                                </td>
+                                <td>
+                                    <button type="submit" className="btn btn-primary">Change</button>
                                 </td>
                             </tr>
-                            //     )}
-                            // </Form>
                         ))}
                     </tbody>
                 </table>
